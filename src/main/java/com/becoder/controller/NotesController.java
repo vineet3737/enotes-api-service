a5b3475094055bd956115e22aa5c032e094841b3
vineet3737 <rvineet29@gmail.com>
2025-06-03 21:58:33 +0530

    uploading file

--- src/main/java/com/becoder/controller/NotesController.java
+++ src/main/java/com/becoder/controller/NotesController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -20,8 +21,9 @@ public class NotesController {
     private NotesService notesService;
 
     @PostMapping("/saveNotes")
-    public ResponseEntity<?> saveNotes(@RequestBody NotesDto notesDto){
-        Boolean saveNotes = notesService.saveNotes(notesDto);
+    public ResponseEntity<?> saveNotes(@RequestParam String notes,
+                                       @RequestParam (required = false) MultipartFile file) throws Exception {
+        Boolean saveNotes = notesService.saveNotes(notes, file);
         if(saveNotes){
             return CommonUtils.createBuildResponseMessage("Notes saved successfully", HttpStatus.CREATED);
         }
--- src/main/java/com/becoder/service/NotesService.java
+++ src/main/java/com/becoder/service/NotesService.java
@@ -1,12 +1,13 @@
 package com.becoder.service;
 
 import com.becoder.dto.NotesDto;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface NotesService {
 
-    public Boolean saveNotes(NotesDto notesDto);
+    public Boolean saveNotes(String notes, MultipartFile file) throws Exception;
 
     public List<NotesDto> getAllNotes();
 }
