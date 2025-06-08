2c7819ac110de32ce5bc9a590e67b07b01bccf31
vineet3737 <rvineet29@gmail.com>
2025-06-08 13:39:03 +0530

    auth

--- src/main/java/com/becoder/controller/AuthController.java
+++ src/main/java/com/becoder/controller/AuthController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/user")
-public class UserController {
+@RequestMapping("/api/v1/auth")
+public class AuthController {
 
     @Autowired
     private UserService userService;
